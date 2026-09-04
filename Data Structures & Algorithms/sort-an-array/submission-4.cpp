6896266067a9decf5cbb84f6533c3eedfb33c749
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-09-04 11:31:01 -0700

    Add: sort-an-array - submission-4

---
+++ Data Structures & Algorithms/sort-an-array/submission-4.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void sort(vector<int>& nums, int left, int right){
+        if(left>=right){
+            return;
+        }
+        int mid = left + (right-left)/2;
+        sort(nums, left, mid);
+        sort(nums,mid+1,right);
+        merge(nums, left, mid,right);
+    }
+    void merge(vector<int>& nums, int left, int mid, int right){
+        int i = left;
+        int j = mid+1;
+        while(i<=mid && j<=right){
+            if(nums[i]<nums[j]){
+                i++;
+            }else{
+                int index = j;
+                int value = nums[j];
+                while(index>i){
+                    nums[index]=nums[index-1];
+                    index--;
+                }
+                nums[i]=value;
+                i++;
+                j++;
+                mid++;
+            }
+            
+        }
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
